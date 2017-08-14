72b8720151ee56fbda8db86ecc912105dc05d5e7
windyear <1280027646@qq.com>
2017-08-14 22:05:18 +0800

    finish the exercise of lesson14

---
+++ lesson14_1.py
@@ -0,0 +1,41 @@
+# By Dimitris_GR from forums
+# Modify Problem Set 31's (Optional) Symmetric Square to return True 
+# if the given square is antisymmetric and False otherwise. 
+# An nxn square is called antisymmetric if A[i][j]=-A[j][i] 
+# for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
+
+def antisymmetric(matrix):
+    #Write your code here
+    if matrix == []:
+        return True
+    length = len(matrix)
+    length_of_column = len(matrix[0])
+    if length != length_of_column:
+        return False
+    for i in range(length):
+        for j in range(length):
+            if matrix[i][j] != -matrix[j][i]:
+                return False
+    return True
+
+# Test Cases:
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, 3], 
+                     [-2, -3, 0]])   
+#>>> True
+
+print antisymmetric([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]])
+#>>> True
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, -2], 
+                     [2, 2,  3]])
+#>>> False
+
+print antisymmetric([[1, 2, 5],
+                     [0, 1, -9],
+                     [0, 0, 1]])
+#>>> False
