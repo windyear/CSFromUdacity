e075a4f7c468114b044fae4d3cf74e2a166d9e9b
windyear <1280027646@qq.com>
2017-08-27 22:22:47 +0800

    learn about index

---
+++ lesson15_3_add_to_index.py
@@ -0,0 +1,40 @@
+# Define a procedure, add_to_index,
+# that takes 3 inputs:
+
+# - an index: [[<keyword>,[<url>,...]],...]
+# - a keyword: String
+# - a url: String
+
+# If the keyword is already
+# in the index, add the url
+# to the list of urls associated
+# with that keyword.
+
+# If the keyword is not in the index,
+# add an entry to the index: [keyword,[url]]
+
+index = []
+
+def add_to_index(index,keyword,url):
+    is_keyword_in_index = False
+    for keyword_in_index in index:
+        if keyword_in_index[0] == keyword:
+            is_keyword_in_index = True
+            keyword_in_index[1].append(url)
+            break
+    if not is_keyword_in_index:
+        index.append([keyword,[url]])
+        
+
+
+
+
+
+add_to_index(index,'udacity','http://udacity.com')
+add_to_index(index,'computing','http://acm.org')
+add_to_index(index,'udacity','http://npr.org')
+print index
+#>>> [['udacity', ['http://udacity.com', 'http://npr.org']], 
+#>>> ['computing', ['http://acm.org']]]
+
+
